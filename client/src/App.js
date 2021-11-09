48ed1ec9895fba5867cb4908202daa7c1ee978dd
macvanthiem <macvanthiem1999@gmail.com>
2021-11-09 23:47:28 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import GlobalStyle from "./GlobalStyle";
 import Login from "./pages/Login";
 import PrivateRoute from "./components/PrivateRoute";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import NotFound from "./pages/NotFound";
 function App() {
     return (
         <BrowserRouter>
             <Switch>
+                <Route exact path='/'>
+                    <Redirect to='/login' />
+                </Route>
                 <Route path='/login' component={Login} />
                 <PrivateRoute path='/dashboard'>
                     <Dashboard />
