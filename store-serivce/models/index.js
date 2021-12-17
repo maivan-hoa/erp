e96731105180eefbf8ecaf0dde134b95025c2667
macvanthiem <macvanthiem1999@gmail.com>
2021-12-17 19:57:47 +0700

    mongodb cloud, dockerfile

--- store-serivce/models/index.js
+++ store-serivce/models/index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const { MONGO_URI } = require("../config");
 
 mongoose.connect(MONGO_URI, {
-    autoIndex: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
